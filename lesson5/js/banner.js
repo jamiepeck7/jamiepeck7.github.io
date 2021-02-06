3e3a5e5ef14cb1a397328615e9781327c84a6ab0
Use your Github username <Enter your email address>
2021-02-06 09:26:48 -0700

    commit

---
+++ lesson5/js/banner.js
@@ -0,0 +1,11 @@
+
+
+
+    
+
+    var date = new Date();
+    if (date.getDay() == 5) {
+       document.getElementById("banner").innerText =
+        "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavillion";
+        document.getElementById("banner").style.display = "block";
+    }
