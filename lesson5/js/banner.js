264cc658c34701293645c3c180a63a617fb53074
Use your Github username <Enter your email address>
2021-02-12 19:05:03 -0700

    commit

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -7,5 +7,8 @@
     if (date.getDay() == 5) {
        document.getElementById("banner").innerText =
         "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavillion";
-        document.getElementById("banner").style.display = "block";
+        document.getElementById("banner").style.backgroundColor = "yellow";
+        document.getElementById("banner").style.textAlign = "center";
+        document.getElementById("banner").style.fontSize = "x-large";
+        
     }
---
+++ lesson6/js/banner.js
@@ -0,0 +1,15 @@
+
+
+
+    
+
+   
+    var date = new Date();
+    if (date.getDay() == 5) {
+       document.getElementById("banner").innerText =
+        "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavillion";
+        document.getElementById("banner").style.backgroundColor = "yellow";
+        document.getElementById("banner").style.textAlign = "center";
+        document.getElementById("banner").style.fontSize = "x-large";
+        
+    }
