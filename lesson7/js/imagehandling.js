9508c8db7c90bb8858e36543698d3e861a29d306
Use your Github username <Enter your email address>
2021-02-16 20:17:59 -0700

    commit

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -4,7 +4,7 @@ const imagesToLoad = document.querySelectorAll('img[data-src]');
 //optional parameters being set for the IntersectionalObserver
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -350px 0px"
+    rootMargin: "0px 0px -400px 0px"
 };
 
 const loadImages = (image) => {
